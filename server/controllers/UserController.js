a16439b936ab4e5b6a7d4e12463a864c2fbe9b24
Much Darmawan Iriansyah Syam <mchdarmawan@gmail.com>
2023-10-12 20:59:11 +0900

    Menambah fungsi update user dan get spesifik user

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,18 +1,18 @@
 import Users from "../models/UserModel.js";
 import argon2 from "argon2";
 export const createUser = async(req, res) => {
-    const {name, email, password, cPassword, role, profil} = req.body;
+    const {nama, email, password, cPassword, role, profil} = req.body;
     if (password !== cPassword) return res.status(400).json({msg: "Password dan Confirm Password tidak cocok."});
     const hashPassowrd = await argon2.hash(password);
     try {
         await Users.create({
-            nama: name,
+            nama: nama,
             email: email,
             password: hashPassowrd,
             role: role,
             fotoprofil: profil
         });
-        res.status(201).json({msg: "Tambah data petugas berhasil."});
+        res.status(201).json({msg: "Data petugas berhasil ditambah."});
     } catch (error) {
         res.status(400).json({msg: error.message});
     }
@@ -26,6 +26,55 @@ export const getUsers = async(req, res) => {
         res.status(200).json(response);
     } catch (error) {
         res.status(500).json({msg: error.message});
+    }
+}
+
+export const getUsersById = async(req, res) => {
+    try {
+        const response = await Users.findOne({
+            attributes: ['uuid', 'nama', 'email', 'role', 'fotoprofil'],
+            where: {
+                uuid: req.params.id
+            }
+        });
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
+}
+
+export const updateUsers = async(req, res) => {
+    const user = await Users.findOne({
+        where: {
+            uuid: req.params.id
+        }
+    });
+
+    if(!user) return res.status(404).json({msg: "User tidak ditemukan."});
+    const {nama, email, password, cPassword, role, profil} = req.body;
+    let hashPassowrd;
+    if(password === "" || password === null){
+        hashPassowrd = user.password
+    }else{
+        hashPassowrd = await argon2.hash(password);
+    }
+    if(password !== cPassword) return res.status(400).json({msg: "Password dan Confirm Password tidak cocok."});
+
+    try {
+        await Users.update({
+            nama: nama,
+            email: email,
+            password: hashPassowrd,
+            role: role,
+            fotoprofil: profil
+        }, {
+            where: {
+                id: user.id
+            }
+        });
+        res.status(200).json({msg: "Data user berhasil diperbarui."});
+    } catch (error) {
+        res.status(400).json({msg: error.message});
     }
 }
 
--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -2,6 +2,8 @@ import express from "express";
 import {
     createUser,
     getUsers,
+    getUsersById,
+    updateUsers,
     deleteUser
 } from "../controllers/UserController.js";
 // middleware
@@ -10,6 +12,8 @@ const router = express.Router();
 
 router.post('/users', createUser);
 router.get('/users', getUsers);
+router.get('/users/:id', getUsersById);
+router.patch('/users/:id', updateUsers);
 router.delete('/users/:id', deleteUser);
 
 export default router;
