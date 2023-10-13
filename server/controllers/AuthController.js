0bbd55e3ff4cdbe413413038f0d2b6ef0eb80e05
Much Darmawan Iriansyah Syam <mchdarmawan@gmail.com>
2023-10-13 22:50:09 +0900

    Menambahkan fungsi get me

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -20,6 +20,21 @@ export const login = async(req, res) => {
     res.status(200).json({uuid, nama, email, role});
 }
 
+export const getMe = async(req, res) => {
+    if(!req.session.userId){
+        return res.status(401).json({msg: "Anda harus login terlebih dahulu"});
+    }
+
+    const user = await Users.findOne({
+        attributes: ['uuid', 'nama', 'email', 'role', 'fotoprofil'],
+        where: {
+            uuid: req.session.userId
+        }
+    });
+    if(!user) return res.status(404).json({msg: "User tidak ditemukan"});
+    res.status(200).json(user);
+}
+
 export const logout = (req, res) => {
     req.session.destroy((err) => {
         if(err) return res.status(400).json({msg: "Tidak dapat logout."});
--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import {login, logout} from "../controllers/AuthController.js"
+import {login, logout, getMe} from "../controllers/AuthController.js"
 
 const router = express.Router();
 
 router.post('/login', login);
+router.get('/me', getMe);
 router.delete('/logout', logout);
 
 export default router;
